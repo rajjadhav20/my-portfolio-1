f4744f4db9df58cd37b60668a48c351a3fbb9e56
Raj Jadhav <91013513+rajjadhav20@users.noreply.github.com>
2024-06-05 13:58:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // specify the output directory
+   "publish": "/opt/build/repo/dist", // specify the output directory
   }
 })
